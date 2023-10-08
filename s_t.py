c306a88563e8733a8121f4a1ad86b3df0991973d
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-10-08 01:15:17 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -6,65 +6,23 @@ import os
 import glob
 import time
 
-# Título de la aplicación
-st.title("Aplicación de Diagnóstico Médico")
-
-# Subir la imagen del diagnóstico
-image_file = st.file_uploader("Cargar imagen del diagnóstico (formato compatible: PNG, JPG, JPEG)", type=["png", "jpg", "jpeg"])
-
-# Definir una función para extraer texto de la imagen y guardar en caché
+# Función para extraer texto de una imagen
 @st.cache
 def extract_text(image):
     text = pytesseract.image_to_string(image)
     return text
 
-# Variable para almacenar el texto extraído
-text = ""
+# Función para generar audio a partir de texto
+def text_to_speech(text, tld):
+    tts = gTTS(text, lang="es", tld=tld, slow=False)
+    try:
+        my_file_name = text[0:20]
+    except:
+        my_file_name = "audio"
+    tts.save(f"temp/{my_file_name}.mp3")
+    return my_file_name
 
-if image_file is not None:
-    # Mostrar la imagen cargada
-    st.image(image_file, caption="Imagen del diagnóstico", use_column_width=True)
-
-    # Botón para extraer el texto de la imagen
-    if st.button("Extraer texto de la imagen"):
-        # Leer la imagen
-        image = Image.open(image_file)
-
-        # Extraer el texto utilizando la función y guardar en caché
-        text = extract_text(image)
-
-        # Mostrar el texto extraído
-        st.subheader("Texto extraído de la imagen:")
-        st.write(text)
-
-# Subir la imagen del diagnóstico
-tld = "com"  # Cambia esto al dominio TLD deseado (p. ej., "es" para español)
-
-# Botón para generar un audio con el texto
-if text:
-    if st.button("Generar audio"):
-        def text_to_speech(text, tld):
-            tts = gTTS(text, lang="es", tld=tld, slow=False)
-            try:
-                my_file_name = text[0:20]
-            except:
-                my_file_name = "audio"
-            tts.save(f"temp/{my_file_name}.mp3")
-            return my_file_name
-
-        # Generar audio con el texto
-        result = text_to_speech(text, tld)
-        audio_file = open(f"temp/{result}.mp3", "rb")
-        audio_bytes = audio_file.read()
-        st.markdown(f"## Audio del diagnóstico:")
-        st.audio(audio_bytes, format="audio/mp3", start_time=0)
-
-        # Botón para eliminar el audio generado
-        if st.button("Eliminar audio generado"):
-            os.remove(f"temp/{result}.mp3")
-            st.success("Audio eliminado correctamente.")
-
-# Botón para eliminar archivos antiguos
+# Función para eliminar archivos antiguos
 def remove_files(n):
     mp3_files = glob.glob("temp/*mp3")
     if len(mp3_files) != 0:
@@ -75,7 +33,66 @@ def remove_files(n):
                 os.remove(f)
                 print("Deleted", f)
 
+# Configuración de la página
+st.set_page_config(
+    page_title="Aplicación de Diagnóstico Médico",
+    page_icon="✅",
+    layout="wide"
+)
+
+# Crear una barra de navegación para cambiar entre las secciones
+selected_page = st.sidebar.radio("Selecciona una opción:", ["Transcripción y Audio", "Reconocimiento de Paciente"])
+
+# Contenido de la página principal
+if selected_page == "Transcripción y Audio":
+    # Título de la aplicación
+    st.title("Transcripción y Generación de Audio")
+
+    # Subir la imagen del diagnóstico
+    image_file = st.file_uploader("Cargar imagen del diagnóstico (formato compatible: PNG, JPG, JPEG)", type=["png", "jpg", "jpeg"])
+
+    # Variable para almacenar el texto extraído
+    text = ""
+
+    if image_file is not None:
+        # Mostrar la imagen cargada
+        st.image(image_file, caption="Imagen del diagnóstico", use_column_width=True)
+
+        # Botón para extraer el texto de la imagen
+        if st.button("Extraer texto de la imagen"):
+            # Leer la imagen
+            image = Image.open(image_file)
+
+            # Extraer el texto utilizando la función y guardar en caché
+            text = extract_text(image)
+
+            # Mostrar el texto extraído
+            st.subheader("Texto extraído de la imagen:")
+            st.write(text)
+
+    # Botón para generar un audio con el texto
+    if text:
+        if st.button("Generar audio"):
+            result = text_to_speech(text, "com")  # Cambia esto al dominio TLD deseado
+            audio_file = open(f"temp/{result}.mp3", "rb")
+            audio_bytes = audio_file.read()
+            st.markdown(f"## Audio del diagnóstico:")
+            st.audio(audio_bytes, format="audio/mp3", start_time=0)
+
+            # Botón para eliminar el audio generado
+            if st.button("Eliminar audio generado"):
+                os.remove(f"temp/{result}.mp3")
+                st.success("Audio eliminado correctamente.")
+
+# Reconocimiento de Paciente
+elif selected_page == "Reconocimiento de Paciente":
+    # Título de la sección
+    st.title("Reconocimiento de Paciente")
+    st.write("¡Aquí puedes agregar la interfaz de reconocimiento de paciente!")
+
+# Limpiar archivos antiguos
 remove_files(7)
+
 
             
 
