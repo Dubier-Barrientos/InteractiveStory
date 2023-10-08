260cf352621c1e6b1b7ebc7c8f36012d2b1f8f7d
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-10-08 00:09:50 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -7,15 +7,10 @@ import speech_recognition as sr
 
 st.title("Conversión de Audio a Texto")
 
-# Variable para almacenar la transcripción
-transcripcion = ""
-grabacion_activa = False  # Indica si la grabación está activa
-
 st.write("Habla y convierte tu discurso en texto:")
 
 stt_button = Button(label=" Iniciar ", width=200)
 
-# Botón para iniciar la grabación
 stt_button.js_on_event("button_click", CustomJS(code="""
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
@@ -35,16 +30,8 @@ stt_button.js_on_event("button_click", CustomJS(code="""
     recognition.start();
     """))
 
-# Botón para detener la grabación
-stop_button = Button(label=" Detener ", width=200)
-
-stop_button.js_on_event("button_click", CustomJS(code="""
-    var recognition = new webkitSpeechRecognition();
-    recognition.stop();
-    """))
-
 result = streamlit_bokeh_events(
-    [stt_button, stop_button],
+    stt_button,
     events="GET_TEXT",
     key="listen",
     refresh_on_update=False,
@@ -53,22 +40,7 @@ result = streamlit_bokeh_events(
 
 if result:
     if "GET_TEXT" in result:
-        transcripcion = result.get("GET_TEXT")
-        st.write(transcripcion)
-        grabacion_activa = False  # La grabación ha finalizado
-
-# Botón para borrar la transcripción y grabar otro audio
-if st.button("Borrar"):
-    transcripcion = ""
-    grabacion_activa = False
-    st.write("Transcripción borrada.")
-
-# Mostrar la transcripción actual si la grabación está activa
-if grabacion_activa:
-    st.subheader("Texto convertido del audio:")
-    st.write("Grabando...")  # Puedes mostrar un mensaje mientras se graba
-
-
+        st.write(result.get("GET_TEXT"))
 
 
 
