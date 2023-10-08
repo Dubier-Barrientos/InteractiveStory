4014d872d3e3f1f01ab01b449cb137b52f7111c4
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-10-07 23:32:50 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -4,30 +4,31 @@ import speech_recognition as sr
 # Título de la aplicación
 st.title("Conversión de Audio a Texto")
 
-# Crear un botón para iniciar la grabación de audio
-if st.button("Iniciar Grabación"):
-    st.write("Habla ahora...")
+# Subir archivo de audio
+audio_file = st.file_uploader("Cargar archivo de audio (formato compatible: WAV, FLAC, etc.)", type=["wav", "flac"])
+
+# Verificar si se ha cargado un archivo
+if audio_file:
+    st.write("Procesando archivo de audio...")
 
     # Crear un objeto Recognizer
     recognizer = sr.Recognizer()
 
-    # Abrir el micrófono
-    with sr.Microphone() as source:
+    # Leer el archivo de audio
+    audio_data = sr.AudioFile(audio_file)
+
+    # Iniciar la conversión de audio a texto
+    with audio_data as source:
         try:
-            # Escuchar el audio
-            audio_data = recognizer.listen(source)
-            st.write("Grabación completada. Procesando...")
-
-            # Convertir el audio a texto utilizando el reconocimiento de Google
-            text = recognizer.recognize_google(audio_data)
-
-            # Mostrar el texto convertido
+            audio_text = recognizer.record(source)
+            text = recognizer.recognize_google(audio_text)
             st.subheader("Texto convertido del audio:")
             st.write(text)
-        except sr.WaitTimeoutError:
-            st.write("No se detectó ninguna entrada de audio.")
+        except sr.UnknownValueError:
+            st.warning("No se pudo reconocer el audio.")
         except sr.RequestError as e:
             st.error(f"Error en la solicitud de reconocimiento de voz: {e}")
+
 
 
 
