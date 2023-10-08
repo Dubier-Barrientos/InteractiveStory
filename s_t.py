22f35fcd571a530007776fa75a126f333c0fe30b
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-10-08 11:05:03 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -20,9 +20,45 @@ selected_page = st.sidebar.radio("Selecciona una opción:", ["Historia a audio",
 
 if selected_page == "Historia a audio":
     
-    # Título de la aplicación
-    st.title("Transcripción y Generación de Audio")
+    text = st.text_input("Ingrese el texto.")
+    tld="es"
 
+    def text_to_speech(text, tld):
+    
+        tts = gTTS(text,"es", tld, slow=False)
+        try:
+            my_file_name = text[0:20]
+        except:
+            my_file_name = "audio"
+        tts.save(f"temp/{my_file_name}.mp3")
+        return my_file_name, text
+
+    
+    if st.button("convertir"):
+        result, output_text = text_to_speech(text, tld)
+        audio_file = open(f"temp/{result}.mp3", "rb")
+        audio_bytes = audio_file.read()
+        st.markdown(f"## Tú audio:")
+        st.audio(audio_bytes, format="audio/mp3", start_time=0)
+    
+        #if display_output_text:
+        st.markdown(f"## Texto en audio:")
+        st.write(f" {output_text}")
+
+
+    
+    def remove_files(n):
+        mp3_files = glob.glob("temp/*mp3")
+        if len(mp3_files) != 0:
+            now = time.time()
+            n_days = n * 86400
+            for f in mp3_files:
+                if os.stat(f).st_mtime < now - n_days:
+                    os.remove(f)
+                    print("Deleted ", f)
+
+    remove_files(7)
+    
 elif selected_page == "Dibujemos una historia":
 
     #Título de la sección
