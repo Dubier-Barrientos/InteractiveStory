3335ec7b0826b7177c0abbb5b7e2882679e94187
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-10-08 01:23:48 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -3,56 +3,52 @@ from PIL import Image
 import pytesseract
 from gtts import gTTS
 import os
-import glob
-import time
+import face_recognition
+import os
 
-# Función para extraer texto de una imagen
+# Definir función para extraer texto de la imagen y guardar en caché
 @st.cache
 def extract_text(image):
     text = pytesseract.image_to_string(image)
     return text
 
-# Función para generar audio a partir de texto
-def text_to_speech(text, tld):
-    tts = gTTS(text, lang="es", tld=tld, slow=False)
-    try:
-        my_file_name = text[0:20]
-    except:
-        my_file_name = "audio"
-    tts.save(f"temp/{my_file_name}.mp3")
-    return my_file_name
+# Definir función para reconocimiento facial
+def facial_recognition(image_file):
+    known_faces = []
+    known_names = []
+    known_faces_folder = "known_faces"
 
-# Función para eliminar archivos antiguos
-def remove_files(n):
-    mp3_files = glob.glob("temp/*mp3")
-    if len(mp3_files) != 0:
-        now = time.time()
-        n_days = n * 86400
-        for f in mp3_files:
-            if os.stat(f).st_mtime < now - n_days:
-                os.remove(f)
-                print("Deleted", f)
+    for filename in os.listdir(known_faces_folder):
+        if filename.endswith(".jpg"):
+            image = face_recognition.load_image_file(os.path.join(known_faces_folder, filename))
+            face_encoding = face_recognition.face_encodings(image)[0]
+            known_faces.append(face_encoding)
+            known_names.append(os.path.splitext(filename)[0])
 
-# Configuración de la página
-st.set_page_config(
-    page_title="Aplicación de Diagnóstico Médico",
-    page_icon="✅",
-    layout="wide"
-)
+    input_image = face_recognition.load_image_file(image_file)
+    face_locations = face_recognition.face_locations(input_image)
+    face_encodings = face_recognition.face_encodings(input_image, face_locations)
 
-# Crear una barra de navegación para cambiar entre las secciones
-selected_page = st.sidebar.radio("Selecciona una opción:", ["Transcripción y Audio", "Reconocimiento de Paciente"])
+    recognized_person = None
 
-# Contenido de la página principal
-if selected_page == "Transcripción y Audio":
-    # Título de la aplicación
-    st.title("Transcripción y Generación de Audio")
+    if len(face_encodings) > 0:
+        for i, face_encoding in enumerate(face_encodings):
+            results = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.5)
+            if True in results:
+                index = results.index(True)
+                recognized_person = known_names[index]
+                break
 
-    # Subir la imagen del diagnóstico
+    return recognized_person
+
+# Menú lateral
+menu_option = st.sidebar.selectbox("Menú", ["Inicio", "Reconocimiento de Pacientes"])
+
+# Cargar imagen del diagnóstico
+if menu_option == "Inicio":
+    st.title("Aplicación de Diagnóstico Médico")
+
     image_file = st.file_uploader("Cargar imagen del diagnóstico (formato compatible: PNG, JPG, JPEG)", type=["png", "jpg", "jpeg"])
-
-    # Variable para almacenar el texto extraído
-    text = ""
 
     if image_file is not None:
         # Mostrar la imagen cargada
@@ -60,38 +56,43 @@ if selected_page == "Transcripción y Audio":
 
         # Botón para extraer el texto de la imagen
         if st.button("Extraer texto de la imagen"):
-            # Leer la imagen
+            # Leer la imagen y extraer el texto
             image = Image.open(image_file)
-
-            # Extraer el texto utilizando la función y guardar en caché
             text = extract_text(image)
 
             # Mostrar el texto extraído
             st.subheader("Texto extraído de la imagen:")
             st.write(text)
 
-    # Botón para generar un audio con el texto
-    if text:
+        # Botón para generar un audio con el texto
         if st.button("Generar audio"):
-            result = text_to_speech(text, "com")  # Cambia esto al dominio TLD deseado
-            audio_file = open(f"temp/{result}.mp3", "rb")
-            audio_bytes = audio_file.read()
+            # Generar audio con el texto
+            tts = gTTS(text, lang="es")  # Puedes especificar el idioma que desees
+            audio_file = "diagnostico_audio.mp3"
+            tts.save(audio_file)
+
+            # Mostrar el enlace para descargar el audio
             st.markdown(f"## Audio del diagnóstico:")
-            st.audio(audio_bytes, format="audio/mp3", start_time=0)
+            st.audio(audio_file, format="audio/mp3", start_time=0)
 
-            # Botón para eliminar el audio generado
-            if st.button("Eliminar audio generado"):
-                os.remove(f"temp/{result}.mp3")
-                st.success("Audio eliminado correctamente.")
+# Reconocimiento de Pacientes
+elif menu_option == "Reconocimiento de Pacientes":
+    st.title("Reconocimiento de Pacientes")
 
-# Reconocimiento de Paciente
-elif selected_page == "Reconocimiento de Paciente":
-    # Título de la sección
-    st.title("Reconocimiento de Paciente")
-    st.write("¡Aquí puedes agregar la interfaz de reconocimiento de paciente!")
+    image_file_patient = st.file_uploader("Cargar imagen del paciente para reconocimiento", type=["png", "jpg", "jpeg"])
 
-# Limpiar archivos antiguos
-remove_files(7)
+    if image_file_patient is not None:
+        # Mostrar la imagen cargada
+        st.image(image_file_patient, caption="Imagen del paciente", use_column_width=True)
+
+        # Realizar el reconocimiento facial
+        recognized_person = facial_recognition(image_file_patient)
+
+        if recognized_person is not None:
+            st.subheader("Paciente Reconocido:")
+            st.write(recognized_person)
+        else:
+            st.warning("Paciente no reconocido.")
 
 
             
