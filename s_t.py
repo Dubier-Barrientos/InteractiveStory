21fd3185643b3288174f6e8f4724ef729abe9a5c
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-10-08 00:57:00 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -32,19 +32,44 @@ if image_file is not None:
         st.subheader("Texto extraído de la imagen:")
         st.write(text)
 
-    # Botón para generar un audio con el texto
-    if st.button("Generar audio"):
-        # Recuperar el texto de la caché
-        text = extract_text(image)
+def text_to_speech(text, tld):
+    #translation = translator.translate(text, src=input_language, dest=output_language)
+    
+    tts = gTTS(text, "es", tld=tld, slow=False)
+    try:
+        my_file_name = text[0:20]
+    except:
+        my_file_name = "audio"
+    tts.save(f"temp/{my_file_name}.mp3")
+    return my_file_name, trans_text
 
-        # Generar audio con el texto
-        tts = gTTS(text, lang="es")  # Puedes especificar el idioma que desees
-        audio_file = "diagnóstico_audio.mp3"
-        tts.save(audio_file)
 
-        # Mostrar el enlace para descargar el audio
-        st.markdown(f"## Audio del diagnóstico:")
-        st.audio(audio_file, format="audio/mp3", start_time=0)
+display_output_text = st.checkbox("Muestra el  texto ")
+
+if st.button("convert"):
+    result, output_text = text_to_speech("es", "es", text, tld)
+    audio_file = open(f"temp/{result}.mp3", "rb")
+    audio_bytes = audio_file.read()
+    st.markdown(f"## Your audio:")
+    st.audio(audio_bytes, format="audio/mp3", start_time=0)
+
+    if display_output_text:
+        st.markdown(f"## Output text:")
+        st.write(f" {output_text}")
+
+
+def remove_files(n):
+    mp3_files = glob.glob("temp/*mp3")
+    if len(mp3_files) != 0:
+        now = time.time()
+        n_days = n * 86400
+        for f in mp3_files:
+            if os.stat(f).st_mtime < now - n_days:
+                os.remove(f)
+                print("Deleted ", f)
+
+
+remove_files(7)
             
 
            
